0d6db3ed6589f8f02522c176c7029dae9692fc4c
Andrei Gramakov <mail@agramakov.me>
2019-06-22 15:09:01 +0300

    num.upd

--- info.py
+++ info.py
@@ -4,7 +4,7 @@ All package info is here. By defaults, opens URL with the repo
 
 info = {
     "name": "fillvars",
-    "version": "1.1.2",
+    "version": "1.1.2.1",
     "description": "Module for copy or unzip a file to cwd",
     "url": "https://github.com/dongrama/fillvars",
     "author": "Andrei Gramakov",
